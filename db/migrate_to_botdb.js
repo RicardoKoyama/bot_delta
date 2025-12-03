d8a49cf87a28a60fc27542a5aea0af9a9f8908bf
RicardoKoyama <koyamaricardo@gmail.com>
2025-12-03 09:56:19 -0300

    fix: trocado estrutura do bd

--- db/migrate_to_botdb.js
+++ db/migrate_to_botdb.js
@@ -109,7 +109,7 @@ function migrarProdutos() {
   return new Promise((resolve, reject) => {
     console.log("📥 Migrando produtos...");
 
-    oldDB.all("SELECT * FROM delta_produtos", (err, rows) => {
+    oldDB.all("SELECT * FROM produtos", (err, rows) => {
       if (err) return reject(err);
 
       if (rows.length === 0) {
