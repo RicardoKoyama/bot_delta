76f3a82b2d4c8243083f0186ff312629a2a5b10e
RicardoKoyama <koyamaricardo@gmail.com>
2025-12-03 14:41:40 -0300

    Versão DEV com melhorias - ajustes 6

--- services/whatsapp/handlers/delta/imagem.js
+++ services/whatsapp/handlers/delta/imagem.js
@@ -84,7 +84,7 @@ function decodeCodigo(filepath) {
     return new Promise((resolve) => {
         try {
             const path = require("path");
-            const INLITE = path.join(__dirname, "../../../inlite/bin/BarcodeReaderCLI");
+            const INLITE = path.join(__dirname, "../../../../inlite/bin/BarcodeReaderCLI");
 
             const cmd = `${INLITE} -type=qr,ean13,ean8,upca,upce,code128,code39 "${filepath}"`;
             console.log("Executando CLI:", cmd);
--- services/whatsapp/handlers/msi/utils/decodeUtils.js
+++ services/whatsapp/handlers/msi/utils/decodeUtils.js
@@ -1,9 +1,33 @@
-const { decodeGS1 } = require('../../../handlers/handleDecodifica'); 
+const { exec } = require('child_process');
+const fs = require('fs');
+const path = require('path');
 
 module.exports = {
-    decodeImage: async(base64) => {
+    decodeImage: async (base64) => {
+        const tmpPath = `/tmp/msi_decode_${Date.now()}.jpg`;
+
         try {
-            return await decodeGS1(base64);
+            // Salva imagem temporária
+            fs.writeFileSync(tmpPath, Buffer.from(base64, 'base64'));
+
+            const cliPath = path.resolve(__dirname, '../../../../inlite/bin/BarcodeReaderCLI');
+
+            const cmd = `${cliPath} -type=qr,ean13,ean8,upca,upce,code128,code39 ${tmpPath}`;
+
+            return await new Promise((resolve) => {
+                exec(cmd, (err, stdout) => {
+                    fs.unlinkSync(tmpPath);
+
+                    if (err) {
+                        console.error("Erro BarcodeReaderCLI:", err);
+                        return resolve(null);
+                    }
+
+                    const match = stdout.match(/(?<=Value: )(\d{8,14})/);
+                    resolve(match ? match[0] : null);
+                });
+            });
+
         } catch (e) {
             return null;
         }
