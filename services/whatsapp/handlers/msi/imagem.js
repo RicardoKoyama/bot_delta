f9fad402b0edaa58b6fcd02165f6a1f4b3c56404
RicardoKoyama <koyamaricardo@gmail.com>
2025-12-03 14:52:35 -0300

    Versão DEV com melhorias - ajustes 9

--- services/whatsapp/handlers/msi/imagem.js
+++ services/whatsapp/handlers/msi/imagem.js
@@ -1,5 +1,5 @@
 const { pool } = require('../../../../services/dbService');
-const { decodeQRCodeImage } = require('../../../handlers/qrHandle');
+const { decodeQRCodeImage } = require('../../../../handlers/qrHandle');
 
 module.exports = {
     process: async (message, accountId, client) => {
